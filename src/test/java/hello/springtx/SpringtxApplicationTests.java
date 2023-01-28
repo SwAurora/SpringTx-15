8372ce3c2996d9c4d5558003276c59469e77f518
SwAurora <rlatjddnr74@gmail.com>
2023-01-28 20:13:33 +0900

    2023-01-28 master(ver1)

---
+++ src/test/java/hello/springtx/SpringtxApplicationTests.java
@@ -0,0 +1,15 @@
+package hello.springtx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtxApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
